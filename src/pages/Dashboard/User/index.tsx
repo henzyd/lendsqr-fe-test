6bb672facc81d3414fb47cffee96afbb779f1b9e
Henzyd <henzydee@gmail.com>
2023-01-03 03:29:37 +0100

    Built the user details page

--- src/pages/Dashboard/User/index.tsx
+++ src/pages/Dashboard/User/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserCards from "./components/UserCards";
 import "./User.style.scss";
 
@@ -11,7 +11,62 @@ import UsersWithSavingsIcon from "../../../assets/icons/users-page/users-with-sa
 import MenuIcon from "../../../assets/icons/users-page/menu-icon.svg";
 import TableFilterIcon from "../../../assets/icons/users-page/table-filter-icon.svg";
 
+type Education = {
+  level: string;
+  employmentStatus: string;
+  sector: string;
+  duration: string;
+  officeEmail: string;
+  monthlyIncome: string[];
+  loanRepayment: string;
+};
+
+type Guarantor = {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  gender: string;
+  address: string;
+};
+
+type Profile = {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  avatar: string;
+  gender: string;
+  bvn: string;
+  address: string;
+  currency: string;
+};
+
+type Socials = {
+  facebook: string;
+  instagram: string;
+  twitter: string;
+};
+
+type userDataArrType = [
+  {
+    accountBalance: string;
+    accountNumber: string;
+    createdAt: string;
+    education: Education;
+    email: string;
+    guarantor: Guarantor;
+    id: string;
+    lastActiveDate: string;
+    orgName: string;
+    phoneNumber: string;
+    profile: Profile;
+    socials: Socials;
+    userName: string;
+  }
+];
+
 const User = () => {
+  const [userDataArr, setUserDataArr] = useState<userDataArrType | []>([]);
+  const [userPathID, setUserPathID] = useState<number | null>(null);
   const cardData = [
     {
       img: {
@@ -75,6 +130,36 @@ const User = () => {
     },
   ];
 
+  useEffect(() => {
+    fetch("https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users")
+      .then((response) =>
+        response.json().then(
+          (data) => setUserDataArr(data)
+          // setUserDataArr((data) => {
+          //   // let changedData: userDataArrType | []; //! FIXME
+          //   // for (let i = 0; i < data.length; i++) {
+          //   //   if (!(i >= 7)) {
+          //   //     changedData.push();
+          //   //   }
+          //   // }
+          //   // console.log(changedData);
+          //   return data;
+          // })
+        )
+      )
+      .catch((error) => console.log(error));
+
+    // *"orgName": "labore-dolor-et",
+    // *"userName": "Wilburn.Rice",
+    // *"email": "Maverick.Hyatt83@gmail.com",
+    // *"phoneNumber": "(553) 208-0727 x31321",
+    // *"createdAt": "2072-12-27T03:44:22.522Z", NOTE: this is the date joined
+  });
+
+  function userPathHandler(id: number) {
+    setUserPathID(id);
+  }
+
   return (
     <div className="UserPage">
       <h1>Users</h1>
@@ -93,59 +178,48 @@ const User = () => {
         ))}
       </div>
 
-      <table className="user-table">
-        <tbody>
-          <tr className="user-table-header">
-            {tableHeadData.map((item) =>
-              item.name === "MENU" ? (
-                <th style={{ visibility: "hidden", padding: "0 12px" }}>
-                  {item.name}
-                </th>
-              ) : (
-                <th>
-                  <div>
+      <div className="table-container">
+        <table className="user-table">
+          <tbody>
+            <tr className="user-table-header">
+              {tableHeadData.map((item) =>
+                item.name === "MENU" ? (
+                  <th
+                    key={item.name}
+                    style={{ visibility: "hidden", padding: "0 12px" }}
+                  >
                     {item.name}
-                    <img src={TableFilterIcon} alt="table-filter-icon" />
-                  </div>
-                </th>
-              )
-            )}
-          </tr>
-          <tr className="user-table-body">
-            <td>Lendsqr</td>
-            <td>Adedeji</td>
-            <td>adedeji@lendsqr.com</td>
-            <td>08078903721</td>
-            <td>May 15, 2020 10:00 AM</td>
-            <td colSpan={2}>Inactive</td>
-            <td>
-              <img src={MenuIcon} alt="menu-icon" />
-            </td>
-          </tr>
-          <tr className="user-table-body">
-            <td>Lendsqr</td>
-            <td>Adedeji</td>
-            <td>adedeji@lendsqr.com</td>
-            <td>08078903721</td>
-            <td>May 15, 2020 10:00 AM</td>
-            <td colSpan={2}>Inactive</td>
-            <td>
-              <img src={MenuIcon} alt="menu-icon" />
-            </td>
-          </tr>
-          <tr className="user-table-body">
-            <td>Lendsqr</td>
-            <td>Adedeji</td>
-            <td>adedeji@lendsqr.com</td>
-            <td>08078903721</td>
-            <td>May 15, 2020 10:00 AM</td>
-            <td colSpan={2}>Inactive</td>
-            <td>
-              <img src={MenuIcon} alt="menu-icon" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  </th>
+                ) : (
+                  <th key={item.name}>
+                    <div>
+                      {item.name}
+                      <img src={TableFilterIcon} alt="table-filter-icon" />
+                    </div>
+                  </th>
+                )
+              )}
+            </tr>
+            {userDataArr.map((item) => (
+              <tr
+                className="user-table-body"
+                key={item.id}
+                onClick={() => userPathHandler(Number(item.id))}
+              >
+                <td>{item.orgName}</td>
+                <td>{item.userName}</td>
+                <td>{item.email}</td>
+                <td>{item.phoneNumber}</td>
+                <td>{item.createdAt}</td>
+                <td colSpan={2}>Inactive</td>
+                <td>
+                  <img src={MenuIcon} alt="menu-icon" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
